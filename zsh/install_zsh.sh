4fdd1aa2ffb48c51e4b1424b41c7bc0cbd61ca2d
ramkumar <ramkalath@gmail.com>
2017-12-12 16:01:29 +0530

    added hwo to install fzf with oh my zsh

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -1,8 +1,16 @@
 # Amme Sharanam
 
-sudo pacman -S zsh tmux wget
+sudo pacman -S zsh tmux wget fzf
 pushd $HOME/
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s /bin/zsh
 cp $HOME/config_files/zsh/.zshrc $HOME/
 cp $HOME/config_files/tmux/.tmux.conf $HOME/
+
+# install fzf with oh-my-zsh
+git clone https://github.com/junegunn/fzf.git ${ZSH}/custom/plugins/fzf
+${ZSH}/custom/plugins/fzf/install --bin
+
+git clone https://github.com/Treri/fzf-zsh.git ${ZSH}/custom/plugins/fzf-zsh
+
+plugins=(... fzf-zsh ...)
