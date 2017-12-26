30cb052b0c09ea11183c839ba6a43effc00a233b
ramkumar <ramkalath@gmail.com>
2017-12-27 01:13:10 +0530

    added c_template snippet

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -3,12 +3,12 @@
 sudo pacman -S zsh tmux wget fzf --noconfirm
 pushd $HOME/
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s /bin/zsh
-ln -s $HOME/config_files/zsh/.zshrc $HOME/.zshrc
+sudo chsh -s /bin/zsh
+sudo ln -s $HOME/config_files/zsh/.zshrc $HOME/.zshrc
 
 # install fzf with oh-my-zsh
 git clone https://github.com/junegunn/fzf.git ${ZSH}/custom/plugins/fzf
-${ZSH}/custom/plugins/fzf/install --bin
+sudo ${ZSH}/custom/plugins/fzf/install --bin
 
 git clone https://github.com/Treri/fzf-zsh.git ${ZSH}/custom/plugins/fzf-zsh
 
