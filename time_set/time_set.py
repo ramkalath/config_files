b4ab782312cac3af9442ff66d2f15a08614e6c07
ramkumar <ramkalath@gmail.com>
2018-01-05 21:59:09 +0530

    modified set time script

--- time_set/time_set.py
+++ time_set/time_set.py
@@ -1,9 +1,9 @@
 import os
 
-os.system("timedatectl set-timezone \"Asia/Kolkata\"")
-os.system("timedatectl >> a.txt")
+os.system("rm a.txt")
+string = "sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\" >> a.txt"
+os.system(string)
 f = open("a.txt", "r")
-time = f.readlines()[0]
-time = time.split(" ")[-2]
+time = f.readlines()[0].split(" ")[4]
 command = "timedatectl set-time " + time
 os.system(command)
