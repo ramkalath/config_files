d4501f79d68f850ab99aa7c163e14b0e90491b7e
ramkalath <ramkalath@gmail.com>
2018-10-26 15:03:12 +0530

    ubuntu script for global system setup

--- global_system_setup.sh
+++ global_system_setup.sh
@@ -1,7 +1,8 @@
-#~/config_files/nvim/./install_ubuntu.sh
-#~/config_files/tmux/./install_ubuntu.sh
-#~/config_files/urxvt/./urxvt_ubuntu_install.sh
-#~/config_files/zsh/./zsh_install_ubuntu.sh
+~/config_files/nvim/./install_ubuntu.sh
+~/config_files/tmux/./install_ubuntu.sh
+~/config_files/urxvt/./urxvt_ubuntu_install.sh
+~/config_files/zsh/./zsh_install_ubuntu.sh
+reboot
 
 
-# ensure u install nvim plugins
+# ensure u install nvim plugins at the end
--- zsh/zsh_install_ubuntu.sh
+++ zsh/zsh_install_ubuntu.sh
@@ -1,9 +1,10 @@
-#sudo apt-get install zsh
-#chsh -s `/bin/zsh`
+sudo apt-get install zsh
 pushd $HOME/
+rm -rf $HOME/.oh-my-zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+rm $HOME/.zshrc
+sudo ln -s $HOME/config_files/zsh/.zshrc $HOME/.zshrc
 pushd $HOME/.oh-my-zsh/themes
 wget https://raw.githubusercontent.com/jimeh/plain.zsh-theme/master/plain.zsh-theme $HOME/.oh-my-zsh/themes
 pushd $HOME/
-rm .zshrc
-sudo ln -s $HOME/config_files/zsh/.zshrc $HOME/.zshrc
+chsh -s `/bin/zsh`
