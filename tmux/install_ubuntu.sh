672a3002c40b4b31319793cab43a2a0b7129a231
ramkalath <ramkalath@gmail.com>
2018-11-27 11:38:45 +0530

    updated tmux

--- tmux/install_ubuntu.sh
+++ tmux/install_ubuntu.sh
@@ -1,2 +1,7 @@
+# remember to hit prefix+I to fetch the other plugins and install. tpm is simply a plugin manager for tmux
+
 sudo apt-get install tmux
 ln -s $HOME/config_files/tmux/.tmux.conf $HOME/.tmux.conf
+
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
