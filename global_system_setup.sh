fa26f5b99a623dbb9c333f881eacffc1f4072501
ramkalath <ramkalath@gmail.com>
2018-12-31 14:08:49 +0530

    dummy commit

--- global_system_setup.sh
+++
@@ -1,9 +0,0 @@
-sh ~/config_files/nvim/./install_ubuntu.sh
-sh ~/config_files/tmux/./install_ubuntu.sh
-sh ~/config_files/urxvt/./urxvt_ubuntu_install.sh
-sh ~/config_files/zsh/./zsh_install_ubuntu.sh
-
-reboot
-
-
-# ensure u install nvim plugins at the end
