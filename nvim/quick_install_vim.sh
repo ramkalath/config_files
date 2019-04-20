6b9bff8ccbf1ad1a76adc1158d690ddf4eb41432
ramkalath <ramkalath@gmail.com>
2019-04-21 00:57:17 +0530

    dummy commit

--- nvim/quick_install_vim.sh
+++ nvim/quick_install_vim.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
-sudo apt-get -y remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
-sudo apt-get -y install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev python3-dev ruby-full git cmake xclip gdb curl
-
-git clone https://github.com/vim/vim $HOME/Downloads/vim
-
-pushd $HOME/Downloads/vim
-./configure \
---enable-multibyte \
---enable-perlinterp=dynamic \
---enable-rubyinterp=dynamic \
---with-ruby-command=/usr/bin/ruby \
---enable-pythoninterp=dynamic \
---with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
---enable-python3interp \
---with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
---enable-luainterp \
---with-luajit \
---enable-cscope \
---enable-gui=auto \
---with-features=huge \
---with-x \
---enable-fontset \
---enable-largefile \
---disable-netbeans \
---enable-fail-if-missing
-make
-sudo make install
-
+sudo apt-get install vim vim-athena xclip
 mkdir ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 wget https://raw.githubusercontent.com/ramkalath/config_files/master/nvim/init_quick_deploy.vim
