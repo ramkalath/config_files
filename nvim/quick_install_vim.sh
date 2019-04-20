dcfc017418a03d5649c8725a5e2892c7e350c2b1
ramkalath <ramkalath@gmail.com>
2019-04-20 23:56:02 +0530

    quick deploy

--- nvim/quick_install_vim.sh
+++ nvim/quick_install_vim.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 sudo apt-get install vim
 mkdir ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 wget https://raw.githubusercontent.com/ramkalath/config_files/master/nvim/init_quick_deploy.vim
-mv init.vim .vimrc
+mv init_quick_deploy.vim .vimrc
