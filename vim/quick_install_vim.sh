65f7acc9e3b713771d984070c201cd9c9ca8055f
ramkalath <ramkalath@gmail.com>
2019-04-25 14:28:30 +0530

    dummy commit

--- vim/quick_install_vim.sh
+++ vim/quick_install_vim.sh
@@ -2,5 +2,5 @@
 sudo apt-get install vim vim-athena xclip curl git
 mkdir ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-wget https://raw.githubusercontent.com/ramkalath/config_files/master/nvim/init_quick_deploy.vim
+wget https://raw.githubusercontent.com/ramkalath/config_files/master/vim/init_quick_deploy.vim
 mv init_quick_deploy.vim .vimrc
