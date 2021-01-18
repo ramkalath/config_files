ea99c51b443df13bcb30df9260d95bfe3e72a711
ramkalath <ramkalath@gmail.com>
2021-01-18 18:28:42 +0530

    created setup notes

--- latex/setup_notes.py
+++ latex/setup_notes.py
@@ -1,14 +1,19 @@
 import os
 import datetime
+import sys
 
 if __name__ == "__main__":
-    time_stamp = str(datetime.datetime.now()).split(" ")[0]
-    os.system("mkdir "+time_stamp)
-    os.chdir(time_stamp)
+    try:
+        myfile = sys.argv[1]
+    except:
+        time_stamp = str(datetime.datetime.now()).split(" ")[0]
+        myfile = time_stamp
+    os.system("mkdir "+myfile)
+    os.chdir(myfile)
     cwd = os.getcwd() + "/"
 
     # create a latex file
-    filename = cwd + time_stamp + ".tex"
+    filename = cwd + myfile + ".tex"
     os.system("touch " + filename)
     file_object = open(filename, 'w')
     file_object.write("\documentclass[10pt]{article} \n\usepackage[letterpaper, portrait, margin=0.5in]{geometry} \n \n\\begin{document} \n\\title{} \n\\author{} \n\date{\\today} \n\maketitle \n\end{document} \n ")
@@ -17,4 +22,4 @@ if __name__ == "__main__":
     filename = cwd + "Makefile"
     os.system("touch " + filename)
     file_object = open(filename, 'w')
-    file_object.write("# enter a filename without the extension \nFILENAME="+time_stamp+"\n\nall:\n\tpdflatex $(FILENAME).tex\n\tbibtex $(FILENAME).aux\n\tpdflatex $(FILENAME).tex\n\tpdflatex $(FILENAME).tex\n\nrun:\n\tbibtex $(FILENAME)\n\tpdflatex $(FILENAME).tex\n\tpdflatex $(FILENAME).tex\n\nclean:\n\trm $(FILENAME).log $(FILENAME).aux *.lof *.blg *.toc *.out *.lot *.bbl\n")
+    file_object.write("# enter a filename without the extension \nFILENAME="+myfile+"\n\nall:\n\tpdflatex $(FILENAME).tex\n\tbibtex $(FILENAME).aux\n\tpdflatex $(FILENAME).tex\n\tpdflatex $(FILENAME).tex\n\nrun:\n\tbibtex $(FILENAME)\n\tpdflatex $(FILENAME).tex\n\tpdflatex $(FILENAME).tex\n\nclean:\n\trm $(FILENAME).log $(FILENAME).aux *.lof *.blg *.toc *.out *.lot *.bbl\n")
