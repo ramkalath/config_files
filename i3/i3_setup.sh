48055e23a20be82a21cebae6a947154d49a5d420
ramkalath <ramkalath@gmail.com>
2021-03-24 22:54:29 +0530

    dummy commit

--- i3/i3_setup.sh
+++ i3/i3_setup.sh
@@ -1,3 +1,4 @@
 sudo apt-get install i3-wm i3blocks i3status i3lock-fancy rofi volumeicon-alsa lxappearance
 
-#sometimes thunar does not display the icons properly. Use lxappearence (xfce4-appfinder->lxappearence (it ll appear as 'Customize Look and Feel')) to setup a proper icon theme for thunar and you are good to go
+# sometimes thunar does not display the icons properly. Use lxappearence (xfce4-appfinder->lxappearence (it ll appear as 'Customize Look and Feel')) to setup a proper icon theme for thunar and you are good to go
+# if that does not work then "mv -f ~/.config/Thunar ~/.config/Thunar_backup"
